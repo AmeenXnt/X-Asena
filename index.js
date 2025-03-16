efb1e99623c6d2ffc24b62dec150dd7d44b858cb
AmeenXnt <ameenserbots@gmail.com>
2025-03-17 01:17:21 +0530

    Mega

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const fs = require("fs").promises;
 const path = require("path");
 const config = require("./config");
 const connect = require("./lib/connection");
-const { loadSession } = require("baileys");
 const io = require("socket.io-client");
 const { getandRequirePlugins } = require("./assets/database/plugins");
+const { File } = require("megajs");
 
 global.__basedir = __dirname; // Set the base directory for the project
 
@@ -25,15 +25,23 @@ const readAndRequireFiles = async (directory) => {
 async function initialize() {
   console.log("X-Asena");
   try {
-    if (config.SESSION_ID && !fs.existsSync("session")) {
-      console.log("loading session from session id...");
-      fs.mkdirSync("./session");
-      const credsData = await loadSession(config.SESSION_ID);
-      fs.writeFileSync(
-        "./session/creds.json",
-        JSON.stringify(credsData.creds, null, 2)
-      );
+    if (config.SESSION_ID && !(await fs.stat("session").catch(() => false))) {
+      console.log("Fetching session from Mega.nz...");
+      await fs.mkdir("session", { recursive: true });
+
+      const fileId = config.SESSION_ID.replace(/KeikoBot⚡|𝐂𝐫𝐨𝐧𝐞𝐱𝐁𝐨𝐭~/, "").trim();
+      let file = File.fromURL(`https://mega.nz/file/${fileId}`);
+      const sessionData = await new Promise((resolve, reject) => {
+        file.download((err, data) => {
+          if (err) return reject(err);
+          resolve(data.toString());
+        });
+      });
+
+      await fs.writeFile("./session/creds.json", sessionData);
+      console.log("✅ Session credentials saved!");
     }
+
     await readAndRequireFiles(path.join(__dirname, "/assets/database/"));
     console.log("Syncing Database");
 
@@ -43,13 +51,15 @@ async function initialize() {
     await readAndRequireFiles(path.join(__dirname, "/assets/plugins/"));
     await getandRequirePlugins();
     console.log("✅ Plugins Installed!");
+
     const ws = io("https://socket.xasena.me/", { reconnection: true });
     ws.on("connect", () => console.log("Connected to server"));
     ws.on("disconnect", () => console.log("Disconnected from server"));
+
     return await connect();
   } catch (error) {
     console.error("Initialization error:", error);
-    return process.exit(1); // Exit with error status
+    return process.exit(1);
   }
 }
 
